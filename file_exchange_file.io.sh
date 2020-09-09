60d13996ca93354fc0cd870b81b669ffc6d1dfb9
nhsung <nhsung@gmail.com>
2020-09-09 17:27:01 +0530

    20200909-4

--- file_exchange_file.io.sh
+++ file_exchange_file.io.sh
@@ -1,1 +1,8 @@
+# text
 curl --data "text=this is a secret pw" https://file.io
+
+# file
+curl -F "file=@test.txt" https://file.io
+
+# file with expiracy duration
+curl -F "file=@test.txt" https://file.io/?expires=1w
